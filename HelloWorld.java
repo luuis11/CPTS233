1b6ad6041402c659d7b539bb6632e681ab51d300
luuis11 <49884588+luuis11@users.noreply.github.com>
2020-09-01 23:43:35 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
-    
+    public static void tempChange(double farne){
+        double celc = 5*(faren-32)/9;
+        
+        System.out.println("Farenheit: "+faren+"\n Celcius: "+celc);
+    }
 
 
 }
