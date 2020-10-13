723ffb98a450b0feaed5ac6f5289dc2d78fa1811
周易 <zhouyi@zhouyideMacBook-Air.local>
2020-10-12 23:45:16 -0700

    homework2

--- MA2.java
+++ MA2.java
@@ -1,6 +1,6 @@
 public class MA2 {
     public static void main(String[] args){
-        System.out.println("jkskdskdsakjdsajkd");
+        
     }
     
 }
@@ -30,20 +30,27 @@ class Btree {
 			return null; 
 		else
 			return this.root.search(k); 
-	} 
-
-
-	// A utility function to insert a new key in the subtree rooted with 
-    // this node. The assumption is, the node must be non-full when this 
-    // function is called 
-    public void insertNonFull(int k); 
-  
-    // A utility function to split the child y of this node. i is index of y in 
-    // child array C[].  The Child y must be full when this function is called 
-    public void splitChild(int i, BTreeNode y); 
-   
+    } 
+    public void insert(int k){
+        if(root == null){
+            root = new BTreeNode(t, true);
+            root.keys[1] = k;
+            root.n = 1;
+        }else{
+            //if root is full
+            if(root.n == 2*t-1){
+                BTreeNode s = new BTreeNode(t, false);
+                s.C[0] = root;
+                s.splitChild(0, root);
+                root = s;
+            }else{
+                root.insertNonFull(k);
+            }
+        }
+    }
   
 } 
+
 
 // A BTree node 
 class BTreeNode { 
@@ -82,9 +89,82 @@ class BTreeNode {
 		if (leaf == false) 
 			C[i].traverse(); 
 	} 
+    // A utility function to insert a new key in the subtree rooted with 
+    // this node. The assumption is, the node must be non-full when this 
+    // function is called 
+    public void insertNonFull(int k){
+        int i = n-1;
+        if(leaf==true){
+            while(i>=0 && keys[i]>k){
+                keys[i+1]=keys[i];
+                i--;
+            }
+            keys[i+1]=k;
+            n=n+1;
+        }else{
+            while(i>=0 && keys[i]>k)
+                i--;
+            if(C[i+1].n == 2*t-1){
+                splitChild(i+1, C[i+1]);
+
+                if(keys[i+1]<k){
+                    i++;
+                }
+            }
+            C[i+1].insertNonFull(k);
+        }
+    }
+  
+    // A utility function to split the child y of this node. i is index of y in 
+    // child array C[].  The Child y must be full when this function is called 
+    public void splitChild(int i, BTreeNode y){
+        BTreeNode newNode = new BTreeNode(y.t, y.leaf);
+        newNode.n = t-1;
+        //moving keys to the new node
+        for(int j=0;j<t-1;j++){
+            newNode.keys[j] = y.keys[j+t];
+        }
+        //adding children to new node
+        if(y.leaf == false){
+            for(int j=0;j<t;j++){
+                newNode.C[j]=y.C[j+t];
+            }
+          }
+
+        y.n = t-1;
+
+        for(int j=n;j>=i+1;j--)
+            C[j+1] = C[j];
+        C[i+1] = newNode;
+        
+        for(int j=n-1;j>=i;j--)
+            keys[j+1] = keys[j];
+        keys[i] = y.keys[t-1];
+
+        n = n+1;
+    }
 
 	// A function to search a key in the subtree rooted with this node. 
-	BTreeNode search(int k) { // returns NULL if k is not present. 
-
-	} 
+	BTreeNode search(BTreeNode root,int k) { // returns NULL if k is not present. 
+        int i=0;
+        while(i<root.n && k>root.keys[i]){
+            i++;
+        }
+        if(i<=root.n && k == root.keys[i]){
+            return root;
+        }
+        if(root.leaf){
+            return null;
+        }
+        else{
+            return search(root.getChild(i), k);
+        }
+    } 
+    BTreeNode getChild(int index){
+        if(index>=C.length){
+            return null;
+        }
+        return C[index];
+    }
+        
 }
